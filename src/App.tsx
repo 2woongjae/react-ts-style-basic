12bae43c950ea3eb96c3d0b871f2b1afdbaf1e23
Mark Lee <mark@protopie.io>
2018-01-20 04:27:49 +0900

    샘플 완성

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2 id="hello" style={{ fontSize: '30px' }}>
+          Hello
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
